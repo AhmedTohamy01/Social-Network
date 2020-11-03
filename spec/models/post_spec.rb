17b8af1b73117959aafbd578db6156cb340d3987
ezeilo-su <ezeilo.su@gmail.com>
2020-11-03 01:02:16 +0100

    Add post model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'post validation' do
+    before do
+      @user = FactoryBot.create(:user)
+      @post = FactoryBot.create(:post, user: @user)
+    end
+
+    context 'with invalid attributes' do
+      it 'should not be valid when user is nil' do
+        @post.user = nil
+        @post.save
+        expect(@post).to_not be_valid
+      end
+
+      it 'should not be valid when there is no content' do
+        @post.content = ' '
+        @post.save
+        expect(@post).to_not be_valid
+      end
+    end
+
+    context 'with valid attributes' do
+      it 'should not be valid when user is nil' do
+        @post.content = 'Lorem ipsum dolor'
+        @post.save
+        expect(@post).to be_valid
+      end
+    end
+  end
 end
