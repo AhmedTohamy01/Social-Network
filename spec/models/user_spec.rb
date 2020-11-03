4238c5f15148e8c35b6a31b04cbfd465dca320ac
ezeilo-su <ezeilo.su@gmail.com>
2020-11-03 10:10:14 +0100

    Update user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,12 @@ RSpec.describe User, type: :model do
           expect(@user).to be_valid
         end
       end
+      it 'should be saved as all lower-case' do
+        mixed_case_email = 'Foo@ExAMPle.CoM'
+        @user.email = mixed_case_email
+        @user.save
+        expect(@user.reload.email).to eq(mixed_case_email.downcase)
+      end
     end
 
     context 'with invalid attributes' do
@@ -36,38 +42,31 @@ RSpec.describe User, type: :model do
         @user.name = ' '
         expect(@user).not_to be_valid
       end
-    end
 
-    it 'should not be valid when email is blank' do
-      @user.email = ' '
-      expect(@user).not_to be_valid
-    end
-
-    it 'should not be valid when name is too long' do
-      @user.name = 'a' * 51
-      expect(@user).not_to be_valid
-    end
-
-    it 'should be invalid when email format is invalid' do
-      addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
-      addresses.each do |invalid_address|
-        @user.email = invalid_address
+      it 'should not be valid when email is blank' do
+        @user.email = ' '
         expect(@user).not_to be_valid
       end
-    end
 
-    it 'should not be valid when email address is already taken' do
-      @user_with_same_email = FactoryBot.create(:user)
-      @user_with_same_email.email = @user.email
-      @user_with_same_email.save
-      expect(@user_with_same_email).not_to be_valid
-    end
+      it 'should not be valid when name is too long' do
+        @user.name = 'a' * 51
+        expect(@user).not_to be_valid
+      end
 
-    it 'should be saved as all lower-case' do
-      mixed_case_email = 'Foo@ExAMPle.CoM'
-      @user.email = mixed_case_email
-      @user.save
-      expect(@user.reload.email).to eq(mixed_case_email.downcase)
+      it 'should be invalid when email format is invalid' do
+        addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+        addresses.each do |invalid_address|
+          @user.email = invalid_address
+          expect(@user).not_to be_valid
+        end
+      end
+
+      it 'should not be valid when email address is already taken' do
+        @user_with_same_email = FactoryBot.create(:user)
+        @user_with_same_email.email = @user.email
+        @user_with_same_email.save
+        expect(@user_with_same_email).not_to be_valid
+      end
     end
   end
 
