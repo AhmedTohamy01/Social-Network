bcfe637e1490120428a5ddf29f52cbaba512f6fb
ezeilo-su <ezeilo.su@gmail.com>
2020-11-12 15:53:37 +0100

    Make current_user eager load

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def current_user
+    @current_user ||= super && User.includes([:friends, :friend_requests, :pending_friends]).find(@current_user.id)
+  end
+
   protected
 
   def configure_permitted_parameters
